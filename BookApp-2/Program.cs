8c58d3f2b7ab5f148dd44d4c98e4a0ac410b1068
Margreet4 <147156878+Margreet4@users.noreply.github.com>
2026-02-03 03:02:07 +0200

    assignment 2

---
+++ BookApp-2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Book
+{
+    public const string BankCode = "BNK001";
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(Book.BankCode);
+    }
+}
